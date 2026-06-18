7990beaa68ce651e10af0ced69cf22671f4b5fa7
jafar-arrazi <jafar33886@gmail.com>
2026-06-18 17:28:13 +0700

    perubahan api key

--- services/krs/config/iae.php
+++ services/krs/config/iae.php
@@ -3,18 +3,18 @@
 return [
     'service_name' => env('SERVICE_NAME', 'KRS-Service'),
     'api_version' => env('API_VERSION', 'v1'),
-    'api_key' => env('IAE_API_KEY', '102022400045'),
+    'api_key' => env('IAE_API_KEY', 'KEY-MHS-109'),
     'minimum_ips' => (float) env('MINIMUM_IPS_KRS', 2.00),
     'external_validation_enabled' => filter_var(env('EXTERNAL_VALIDATION_ENABLED', true), FILTER_VALIDATE_BOOLEAN),
 
     'services' => [
         'mahasiswa' => [
             'url' => rtrim(env('MAHASISWA_SERVICE_URL', 'http://mahasiswa-service:8000'), '/'),
-            'key' => env('MAHASISWA_SERVICE_KEY', env('IAE_API_KEY', '102022400045')),
+            'key' => env('MAHASISWA_SERVICE_KEY', env('IAE_API_KEY', 'KEY-MHS-109')),
         ],
         'kurikulum_nilai' => [
             'url' => rtrim(env('KURIKULUM_NILAI_SERVICE_URL', 'http://kurikulum-nilai-service:8000'), '/'),
-            'key' => env('KURIKULUM_NILAI_SERVICE_KEY', env('IAE_API_KEY', '102022400045')),
+            'key' => env('KURIKULUM_NILAI_SERVICE_KEY', env('IAE_API_KEY', 'KEY-MHS-109')),
         ],
     ],
 
--- services/mahasiswa/config/iae.php
+++ services/mahasiswa/config/iae.php
@@ -4,7 +4,7 @@ return [
     'services' => [
         'krs' => [
             'url' => rtrim(env('KRS_SERVICE_URL', 'http://krs-service:8000'), '/'),
-            'key' => env('KRS_SERVICE_KEY', '102022400045'),
+            'key' => env('KRS_SERVICE_KEY', 'KEY-MHS-109'),
         ],
         'kurikulum_nilai' => [
             'url' => rtrim(env('KURIKULUM_NILAI_SERVICE_URL', 'http://kurikulum-nilai-service:8000'), '/'),
